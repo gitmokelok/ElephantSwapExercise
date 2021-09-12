734852c96d278e1fb8a227b4f9c53a2f587a7a9c
Dima <mokelok@mail.ru>
2021-09-12 17:52:13 +0200

    Add project files.

---
+++ ElephantSwapExercise/Elephant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElephantSwapExercise
+{
+    class Elephant
+    {
+        public string Name;
+        public int EarSize;
+
+        public void WhoAmI() 
+        {
+            Console.WriteLine("Calling " + this.Name + ".WhoAmI()");
+            Console.WriteLine("My name is " + this.Name + ".");
+            Console.WriteLine("My ears are " + EarSize + " inches tall.");
+        }
+    }
+}
---
+++ ElephantSwapExercise/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ElephantSwapExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elephant lucinda = new Elephant() {Name="Lucinda", EarSize=33 };
+            Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+
+            while (true) 
+            {
+                Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+                char userInput = Console.ReadKey(true).KeyChar;
+
+                Console.WriteLine("You pressed " + userInput);
+
+                if (userInput == '1')
+                {
+                    lloyd.WhoAmI();
+                }
+                else if (userInput == '2')
+                {
+                    lucinda.WhoAmI();
+                }
+                else if (userInput == '3')
+                {
+                    Elephant tempElephant;
+                    tempElephant = lloyd;
+                    lloyd = lucinda;
+                    lucinda = tempElephant;
+                    Console.WriteLine("References have been swapped.");
+                }
+                else
+                {
+                    throw new ArgumentException("Unknown input from the user.");
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
